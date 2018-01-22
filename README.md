f69e9e52424ad56a3a57c23e85d84601c61bf23d
neilwoodroffe <neilowoodroffe@hotmail.com>
2018-01-22 22:11:30 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Falls Risk Screen
+
+This is a falls risk screen that can be completed by anyone who is concerned about having a fall. The report can be printed or saved as a PDF and given to a doctor to aid in the development of a treatment plan.
---
+++ script.js
@@ -0,0 +1,148 @@
+/*This Falls Risk Screen was developed by Neil Woodroffe, Occupational Therapist in 2018*/
+/*Feel free to use/modify this application using the MIT license*/
+
+function showhideForm1(showform1) {
+    if (showform1 == "yes1") {
+        document.getElementById("div1").style.display = 'block';
+        document.getElementById("div2").style.display = 'none';
+} 
+    if (showform1 == "no1") {
+        document.getElementById("div2").style.display = 'block';
+        document.getElementById("div1").style.display = 'none';
+}
+}
+function showhideForm2(showform2) {
+    if (showform2 == "yes2") {
+        document.getElementById("div3").style.display = 'block';
+        document.getElementById("div4").style.display = 'none';
+} 
+    if (showform2 == "no2") {
+        document.getElementById("div4").style.display = 'block';
+        document.getElementById("div3").style.display = 'none';
+}
+}
+function showhideForm3(showform3) {
+    if (showform3 == "yes3") {
+        document.getElementById("div5").style.display = 'block';
+        document.getElementById("div6").style.display = 'none';
+} 
+    if (showform3 == "no3") {
+        document.getElementById("div6").style.display = 'block';
+        document.getElementById("div5").style.display = 'none';
+}
+}
+function showhideForm4(showform4) {
+    if (showform4 == "yes4") {
+        document.getElementById("div7").style.display = 'block';
+        document.getElementById("div8").style.display = 'none';
+} 
+    if (showform4 == "no4") {
+        document.getElementById("div8").style.display = 'block';
+        document.getElementById("div7").style.display = 'none';
+}
+}
+function showhideForm5(showform5) {
+    if (showform5 == "yes5") {
+        document.getElementById("div9").style.display = 'block';
+        document.getElementById("div10").style.display = 'none';
+} 
+    if (showform5 == "no5") {
+        document.getElementById("div10").style.display = 'block';
+        document.getElementById("div9").style.display = 'none';
+}
+}
+function showhideForm6(showform6) {
+    if (showform6 == "yes6") {
+        document.getElementById("div11").style.display = 'block';
+        document.getElementById("div12").style.display = 'none';
+} 
+    if (showform6 == "no6") {
+        document.getElementById("div12").style.display = 'block';
+        document.getElementById("div11").style.display = 'none';
+}
+}
+function showhideForm7(showform7) {
+    if (showform7 == "yes7") {
+        document.getElementById("div13").style.display = 'block';
+        document.getElementById("div14").style.display = 'none';
+} 
+    if (showform7 == "no7") {
+        document.getElementById("div14").style.display = 'block';
+        document.getElementById("div13").style.display = 'none';
+}
+}
+function showhideForm8(showform8) {
+    if (showform8 == "yes8") {
+        document.getElementById("div15").style.display = 'block';
+        document.getElementById("div16").style.display = 'none';
+} 
+    if (showform8 == "no8") {
+        document.getElementById("div16").style.display = 'block';
+        document.getElementById("div15").style.display = 'none';
+}
+}
+function showhideForm9(showform9) {
+    if (showform9 == "yes9") {
+        document.getElementById("div17").style.display = 'block';
+        document.getElementById("div18").style.display = 'none';
+} 
+    if (showform9 == "no9") {
+        document.getElementById("div18").style.display = 'block';
+        document.getElementById("div17").style.display = 'none';
+}
+}
+function showhideForm10(showform10) {
+    if (showform10 == "yes10") {
+        document.getElementById("div19").style.display = 'block';
+        document.getElementById("div20").style.display = 'none';
+} 
+    if (showform10 == "no10") {
+        document.getElementById("div20").style.display = 'block';
+        document.getElementById("div19").style.display = 'none';
+}
+}
+function showhideForm11(showform11) {
+    if (showform11 == "yes11") {
+        document.getElementById("div21").style.display = 'block';
+        document.getElementById("div22").style.display = 'none';
+} 
+    if (showform11 == "no11") {
+        document.getElementById("div22").style.display = 'block';
+        document.getElementById("div21").style.display = 'none';
+}
+}
+function showhideForm12(showform12) {
+    if (showform12 == "yes12") {
+        document.getElementById("div23").style.display = 'block';
+        document.getElementById("div24").style.display = 'none';
+} 
+    if (showform12 == "no12") {
+        document.getElementById("div24").style.display = 'block';
+        document.getElementById("div23").style.display = 'none';
+}
+}
+function showhideForm13(showform13) {
+    if (showform13 == "yes13") {
+        document.getElementById("div25").style.display = 'block';
+        document.getElementById("div26").style.display = 'none';
+} 
+    if (showform13 == "no13") {
+        document.getElementById("div26").style.display = 'block';
+        document.getElementById("div25").style.display = 'none';
+}
+}
+function showhideForm14(showform14) {
+    if (showform14 == "yes14") {
+        document.getElementById("div27").style.display = 'block';
+        document.getElementById("div28").style.display = 'none';
+        document.getElementById("divfinal").style.display = 'block';
+} 
+    if (showform14 == "no14") {
+        document.getElementById("div28").style.display = 'block';
+        document.getElementById("div27").style.display = 'none';
+        document.getElementById("divfinal").style.display = 'block';
+}
+}
+function myFunction() {
+    window.print();
+}
